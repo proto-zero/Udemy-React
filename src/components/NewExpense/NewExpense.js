57b7b2d71779eb0d5612389a9ae54d6eaabf0817
Sean Lenhart <theseanlenhart@gmail.com>
2021-11-28 21:03:19 -0500

    Add ExpenseForm JS and CSS, add styling for ExpenseForm and NewExpense, fill out ExpenseForm html

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './NewExpense.css';
 
 function NewExpense() {
-    return <div>
-        <form></form>
+    return <div className="new-expense">
+        
     </div>
 }
 
