4fe1d8c5b1b34fbe666ab3ec430cc67f3b3dc543
Sean Lenhart <theseanlenhart@gmail.com>
2022-01-12 15:31:54 -0500

    add expenseslist files, create new component for displaying expenses or lack thereof

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
 import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -15,28 +15,15 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem 
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
-    return (
-        <Card className="expenses">
-          <ExpenseFilter 
-            selected={filteredYear} 
-            onChangeFilter={filterChangeHandler} 
-          />
-          {expensesContent}
-        </Card>
-    );
+  return (
+      <Card className="expenses">
+        <ExpenseFilter 
+          selected={filteredYear} 
+          onChangeFilter={filterChangeHandler} 
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+  );
 }
 
 export default Expenses;
